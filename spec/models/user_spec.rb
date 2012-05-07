492e6c0bace322039407841be4cd12180759db70
undeadking <undeadking@gmail.com>
2012-05-07 16:31:04 +0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,8 @@ describe User do
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
 
   it { should be_valid }
 
@@ -119,5 +121,9 @@ describe User do
     end
   end
   
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
 end
